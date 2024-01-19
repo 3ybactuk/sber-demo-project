6e8de4622cdf93f764427e09e1b5b4a9ee5a8162
3ybacTuK <3ybactuk1337@gmail.com>
2024-01-19 18:50:21 +0300

    initial commit

---
+++ src/main/java/com/sber/demo/sberdemoproject/SberDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sber.demo.sberdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sber/demo/sberdemoproject/SberDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sber.demo.sberdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
