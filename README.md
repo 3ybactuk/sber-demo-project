11f641c339883acad7f5568de0405401de589f61
3ybactuk <3ybactuk1337@gmail.com>
2024-01-20 14:19:11 +0300

    added swagger and readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # sber-demo-project
+
+### Swagger документация
+
+UI:     http://localhost:8080/swagger-ui/index.html
+
+JSON:   http://localhost:8080/v3/api-docs
+
+## Запуск приложения
+### Требования
+* Docker
+* docker-compose
+
+### Запуск
+Сборка и запуск осуществляется командой
+
+`make docker_full_up`
--- src/main/java/com/sber/demo/sberdemoproject/SberDemoProjectApplication.java
+++ src/main/java/com/sber/demo/sberdemoproject/SberDemoProjectApplication.java
@@ -5,14 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableJpaRepositories("com.sber.demo.sberdemoproject.*")
 @ComponentScan(basePackages = { "com.sber.demo.sberdemoproject.*" })
 @EntityScan("com.sber.demo.sberdemoproject.*")
-@EnableSwagger2
-@EnableWebMvc
 @SpringBootApplication
 public class SberDemoProjectApplication {
 
